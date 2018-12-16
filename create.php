e2065c8d83c39156e8053aca12dabdf3e89849bd
harish98r <32276096+harish98r@users.noreply.github.com>
2018-12-16 17:42:47 +0530

    Add files via upload

---
+++ create.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+?>
+<html>
+	<body>
+	<center style="color:olive"><h1 style="font-size:10vw">Inmar</h1>
+	</center>
+	<?php
+		$v=$_SESSION["p"];
+		//$e=$_SESSION["e"];
+		echo $v;
+		$r=mysqli_connect("localhost","root","","inmar");
+		$q=mysqli_query($r,"select * from store where pid='$v';");
+		$c=mysqli_num_rows($q);
+			if($c>0)
+				{
+					echo "Sorry! You already have a store. You're not allowed to create another one";
+				}
+			else
+				{
+					mysqli_query($r,"insert into store values('$v');");
+					$sql="CREATE TABLE " . $v . "fruits ( fruitName varchar(20) unique, quantity int, price double unique, quantitySelected int default 0, priceOfSelected double default 0 );";
+					$sql1=mysqli_query($r,$sql);
+					$sql2="insert into " . $v . "fruits (fruitName) values ('mango'),('orange'),('apple'),('banana'),('pineapple'),('pomogranate'),('watermelon'),('guava');";
+					$sql3=mysqli_query($r,$sql2);
+					if($sql1){
+					echo "Youve successfully created your store please update prices.";
+				}
+			else
+				{
+					echo "error creating table";
+				}
+				}
+?>
+	</body>
+</html>
---
+++ updateQuantity.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+?>
+<html>
+<body>
+<center style="color:olive"><h1 style="font-size:10vw">Inmar</h1>
+<?php
+$v=$_SESSION["p"];
+$a=$_POST['a'];
+$b=$_POST['b'];
+//echo $v;
+$r=mysqli_connect("localhost","root","","inmar");
+$sql="update " . $v . "fruits set quantity='$b' where fruitName='$a'";
+$q=mysqli_query($r,$sql);
+if($q)
+	echo "updated";
+else 
+	echo "update failed";
+//$c=mysqli_num_rows($q);
+?>
+</center>
+</body>
+</html>
+
